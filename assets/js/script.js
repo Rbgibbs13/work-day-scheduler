69d157a1f676c3f436ec51ec1730c6cf54e5b645
unknown <rhett.gibbs@yahoo.com>
2024-01-05 22:22:48 -0800

    Last few updates - clearing whitespace and cleaning some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,6 @@ $(function () {
     var btn = this;
     SaveToLocalStorage(btn);
   });
-
-  
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -82,19 +80,18 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  const SaveToLocalStorage = (inBtn) => {
-    var textArea = $(inBtn.parentElement).children('textarea');
-    var keyName = $(inBtn.parentElement).attr("id");
-
-    console.log(keyName + " : " + textArea.val());
-    localStorage.setItem(keyName, textArea.val());
-  };
 
   const PopulateFromLocalStorage = () => {
     for(let i = 0; i < timeBlocks.length; i++) {
       var keyName = $(timeBlocks[i]).attr('id');
       $(timeBlocks[i]).children('textarea').text(localStorage.getItem(keyName));
     }
+  };
+
+  const SaveToLocalStorage = (inBtn) => {
+    var textArea = $(inBtn.parentElement).children('textarea');
+    var keyName = $(inBtn.parentElement).attr("id");
+    localStorage.setItem(keyName, textArea.val());
   };
 
   const FormatDateTime = () => {
